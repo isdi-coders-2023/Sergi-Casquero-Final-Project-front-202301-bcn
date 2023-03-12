581c7c95a3571b3d706b99cf3224919dc40d1fef
Sergi Casquero <sergicasquero@gmail.com>
2023-03-12 22:19:23 +0100

    Add router path list

---
+++ src/router/routers.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter, RouteObject } from "react-router-dom";
+import App from "../App";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import RegisterPage from "../pages/RegisterPage/RegisterPage";
+
+export const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/user/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/user/register",
+        element: <RegisterPage />,
+      },
+    ],
+  },
+];
+
+export const router = createBrowserRouter(routes);
