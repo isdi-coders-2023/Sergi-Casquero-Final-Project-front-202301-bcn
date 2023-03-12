699bb8341e1ca39833d144ea74043b0f655197dd
Sergi Casquero <sergicasquero@gmail.com>
2023-03-12 18:07:08 +0100

    Add Loader structure

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,50 @@
+import LoaderStyled from "./LoaderStyled";
+
+const Loader = (): JSX.Element => {
+  return (
+    <LoaderStyled>
+      <h2 className="loader-title">RockFit</h2>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        style={{
+          margin: "auto",
+          background: "none",
+          display: "block",
+          shapeRendering: "auto",
+        }}
+        width="200px"
+        height="200px"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid"
+      >
+        {[0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330].map(
+          (angle, i) => (
+            <g transform={`rotate(${angle} 50 50)`} key={i}>
+              <rect
+                x="47"
+                y="24"
+                rx="2.4"
+                ry="2.4"
+                width="6"
+                height="12"
+                fill="#d0fd3e"
+              >
+                <animate
+                  attributeName="opacity"
+                  values="1;0"
+                  keyTimes="0;1"
+                  dur="1s"
+                  begin={`${-0.9166666666666666 + i * 0.08333333333333333}s`}
+                  repeatCount="indefinite"
+                />
+              </rect>
+            </g>
+          )
+        )}
+      </svg>
+    </LoaderStyled>
+  );
+};
+
+export default Loader;
