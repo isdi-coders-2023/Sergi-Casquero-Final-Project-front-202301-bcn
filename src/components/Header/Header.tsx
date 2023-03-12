a5c424daaac15a0fcc65cb3b1122784a2313aeea
Sergi Casquero <sergicasquero@gmail.com>
2023-03-12 20:24:06 +0100

    Add Header component structure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import HeaderStyled from "./HeaderStyled";
+
+const Header = (): JSX.Element => {
+  return (
+    <HeaderStyled>
+      <h1 className="logo">RockFit</h1>
+    </HeaderStyled>
+  );
+};
+
+export default Header;
